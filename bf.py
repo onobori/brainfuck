0029b6d5e12f6cdc28b2e1a0ac829c98c2d5cc8c
Shingo Onobori <onoborishingo@gmail.com>
2017-03-30 13:36:26 +0000

    take a rest

--- bf.py
+++ bf.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# using: Python 3.5.3
 
 import sys
+import binascii
 
 def manual(args):
     if (args[1] == '-h') or (args[1] == '--help'):
@@ -25,17 +27,44 @@ def argcheck(args):
 
 def brainfuck(args):
     #manual(args)
-    f = open(args[1], 'r')
-    line = f.readline()
-    #for l in line:
-    while line:
-        print(line.rstrip())
-        #print(line)
+    with open(args[1], 'r') as f:
         line = f.readline()
-    f.close()
+        #for l in line:
+        # with open('test.txt', 'wb') as g:
+        #count(line)
+        while line:
+            #data = bytearray(line)
+            #print(data)
+            #with open('test.txt', 'wb') as g:
+            #g.write(data)
+            #g.close()
+            #lineedit(line)
+            #for l in line:
+            #     if l == '+':
+            #         d = l.replace('+', '1')
+            #         g.write(ord(d))
+            #print(line.rstrip())
+            y = []
+            for l in line:
+                y.append(hex(ord(l)))
+                # data = bytearray(y)
+                #print(y)
+                g = openwrite(data)
+                g.write(data)
+                # for z in y:
+                #     print(z)
+                #     d = bytearray(z)
+                #     g.write(d)
 
-def count(line):
-    pass
+            #g.write(d)
+            line = f.readline()
+
+
+#def lineedit(line):
+#    #line2 = line.rstrip().replace('+','A')
+#    line2 = line.rstrip().replace('+','01')
+#    f.write(ch
+    
 
 def main():
     args = sys.argv
