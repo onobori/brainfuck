b84a5a5ff60e0beca7702c68bbc9450203a36c49
Shingo Onobori <onoborishingo@gmail.com>
2017-03-31 14:18:15 +0000

    remove extra code

--- bf.py
+++ bf.py
@@ -19,45 +19,14 @@ def argcheck(args):
 def token(args):
     with open(args[1], 'r') as f:
         line = f.readline()
-        #for l in line:
-        # with open('test.txt', 'wb') as g:
-        #count(line)
         while line:
-            #data = bytearray(line)
-            #print(data)
-            #with open('test.txt', 'wb') as g:
-            #g.write(data)
-            #g.close()
-            #lineedit(line)
-            #for l in line:
-            #     if l == '+':
-            #         d = l.replace('+', '1')
-            #         g.write(ord(d))
-            #print(line.rstrip())
             y = []
             for l in line:
-                #y.append(hex(ord(l)))
-                #y.append(ord(l))
                 y.append(l)
-                # data = bytearray(y)
-                #print(y)
-                # f = openwrite(data)
-                # f.write(data)
-                # for z in y:
-                #     print(z)
-                #     d = bytearray(z)
-                #     g.write(d)
 
-            #g.write(d)
             print(y)
             line = f.readline()
 
-
-#def lineedit(line):
-#    #line2 = line.rstrip().replace('+','A')
-#    line2 = line.rstrip().replace('+','01')
-#    f.write(ch
-    
 
 def main():
     args = sys.argv
