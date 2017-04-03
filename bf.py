b25c079110fa3a4be553df6f3dac4ac3d8b45bb5
Shingo Onobori <onoborishingo@gmail.com>
2017-04-03 14:49:26 +0000

    remove other files

--- bf.py
+++ bf.py
@@ -4,15 +4,13 @@
 
 import sys
 
-# x = []
-# i = 0
-# #listのインデックスをポインタとして使う
-# ptr = 0
-
-def argcheck(args):
+def main(args):
+    # option is unimplemented
     if len(args) < 2:
         print('''
-    file not found.
+brainfuck interpreter: ver 0.9.0
+usage:
+ python bf_nonloop.py [option] file
     ''')
     elif len(args) > 3:
         print('''
@@ -21,122 +19,42 @@ def argcheck(args):
     else:
         token(args)
 
+# 1行ずつtokenに分割してリストに保存する
 def token(args):
-    # with open(args[1], 'rb') as f:
     with open(args[1], 'r') as f:
-        # g = open(args[2], 'wb')
         line = f.readline()
         list = []
         while line:
-            # list = []
             for l in line:
                 list.append(l)
-                # g.write(l)
-                # list.append(hex(ord(l)))
-                # g.write(bytes(l))
-                # g.write(int(ord(l)))
-
-            # print(list)
             line = f.readline()
-        # print(y)
-        calc(list)
-        # print(args[2])
-        # g.close()
+        brainfuck(list)
 
-# def main(l):
-#     pass
-# 
-# def loop(l):
-#     if (l == '[') and (l != ']'):
-#         calc(l)
-#     elif (l != '[') and (l == ']'):
-#         pass
-# 
-
-def calc(list):
+def brainfuck(list):
     x = []
     data = 0
     #listのインデックスをポインタとして使う
     ptr = 0
-    for l in list:
-        count += 1
-        if l == '+':
+    i = 0
+    for i in range(len(list)):
+        if list[i] == '+':
             data += 1
-        # x.append(hex(ord(l)))
-        # print(hex(ord(l)))
-        elif l == '-':
+        elif list[i] == '-':
             data -= 1
-        elif l == '.':
+        elif list[i] == '.':
             x.append(data)
             print(chr(data), end="")
-        # elif l == ',':
-        #     print(chr(data), end="")
-        elif l == '[':
-        # # 別のリストを作成して[]内の処理を保存する
-            looplist = []
-            if l != ']':
-                looplist.append(l)
-            else:
-                print("looplist:", end="")
-                print(looplist)
-            
-        #     x.append(data)
-        #     while x[ptr]:
-        #         if l == '+':
-        #             data += 1
-        #         # x.append(hex(ord(l)))
-        #         # print(hex(ord(l)))
-        #         elif l == '-':
-        #             data -= 1
-        #         elif l == '.':
-        #             x.append(data)
-        #             print(chr(data), end="")
-        #         elif l == '>':
-        #             ptr += 1
-        #         elif l == '<':
-        #             ptr -= 1
-        #         elif l == ']':
-        #             if x[ptr] == 0:
-        #                 break
-        #             else:
-        #                 continue
-        #         else:
-        #             pass
-        #     # x.append(data)
-        #     # print(x)
-
-        #     # print(x[ptr])
-        #     # print("start [")
-        #     # print(x[ptr])
-        #     # print(ptr)
-        #     # print("end [")
-        #     # while x[ptr]:
-        #     #     # x[ptr] = x[ptr] - 1
-        #     #     pass
-        # elif l == ']':
-        # x[ptr]がゼロでないなら[で保存した処理を繰り返す
-        #     pass
-        #     # if x[ptr] != 0:
-        #     #     x[ptr] = x[ptr] - 1
-        elif l == '>':
+        elif list[i] == '>':
             x.append(0)
             ptr += 1
-        elif l == '<':
+        elif list[i] == '<':
             ptr -= 1
         else:
             pass
 
     print("")
-    print("looplist:", end="")
-    print(looplist)
-    print(list)
-    print("")
-    #print(x)
-    #print(len(x))
-    # print(data)
-    # print(chr(data))
 
 
 if __name__ == '__main__':
     args = sys.argv
-    argcheck(args)
+    main(args)
